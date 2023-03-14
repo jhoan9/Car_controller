418b711e217095d21ecd24263c34b0424c95358e
jhoan9 <69372790+jhoan9@users.noreply.github.com>
2023-03-14 10:05:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carController',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
