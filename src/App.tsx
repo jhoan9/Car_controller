cffbb79c96ff6efb5b3ce8a430ea9659fbf57392
Jhoan Castro <jcassant@everis.com>
2024-03-17 16:12:34 -0500

    se eliminan paginas no usadas

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-import ESP32Component from './pages/ESP32Component';
-import ConnectEsp from './pages/ConnectEsp';
+import { square } from 'ionicons/icons';
 import BluetoothEsp from './pages/ConnectBluetooth/BluetoothEsp'
 
 /* Core CSS required for Ionic components to work properly */
@@ -44,45 +39,17 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
           <Route exact path="/connectEsp32">
             <BluetoothEsp />
-          </Route>
-          <Route exact path="/bluetoothEsp">
-            <ConnectEsp />
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="bluetoothEsp" href="/bluetoothEsp">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 4</IonLabel>
-          </IonTabButton>
+        <IonTabBar slot="bottom">        
           <IonTabButton tab="connectEsp32" href="/connectEsp32">
             <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 5</IonLabel>
+            <IonLabel>Car Controller</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
